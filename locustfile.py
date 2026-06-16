9a6770868a4033f4d27062eebb60fe92ffd1ae0a
Elviskc <elvis8.ek@gmail.com>
2026-06-16 10:59:28 -0300

    Instalação e configuração do sistema

---
+++ locustfile.py
@@ -0,0 +1,9 @@
+﻿from locust import HttpUser, task, between
+
+
+class UsuarioReserva(HttpUser):
+    wait_time = between(1, 3)
+
+    @task
+    def verificar_api_online(self):
+        self.client.get("/ping", name="GET /ping")
