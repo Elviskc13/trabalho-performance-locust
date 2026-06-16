3df86c08395a36c81e9b795b80abf60997e3457f
Elviskc <elvis8.ek@gmail.com>
2026-06-16 13:45:02 -0300

    Adicionando  primeiros cenarios de consulta no Locust

--- locustfile.py
+++ locustfile.py
@@ -7,3 +7,14 @@ class UsuarioReserva(HttpUser):
     @task
     def verificar_api_online(self):
         self.client.get("/ping", name="GET /ping")
+
+    @task(5)
+    def listar_reservas(self):
+        self.client.get("/booking", name="GET /booking")
+
+    @task(3)
+    def filtrar_reservas_por_data(self):
+        self.client.get(
+            "/booking?checkin=2022-01-01&checkout=2022-12-31",
+            name="GET /booking com filtro por data"
+        )
